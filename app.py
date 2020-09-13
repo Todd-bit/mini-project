b8b433b5c6caf4fb7dc587eb4ff55511104e0023
Todd-bit <todd7385@gmail.com>
2020-09-13 14:46:03 +0000

    Recommitting app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def hello():
     return "Hello World...again!"
 
 if __name__ == "__main__":
-    app.run(host = os.environ.get("IP")),
+    app.run(host = os.environ.get("IP"),
             port = int(os.environ.get("PORT")),
             debug = True)
                
