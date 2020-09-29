008fa5695fce55a5781de79a189fe60edf14ec22
Todd-bit <todd7385@gmail.com>
2020-09-29 23:10:19 +0000

    New Task

--- app.py
+++ app.py
@@ -93,6 +93,10 @@ def logout():
     test.insert({"example":"testing"})
     return redirect(url_for('get_tasks'))
 
+@app.route("/add_task") 
+def add_task():
+    return render_template("add_task.html")  
+
 if __name__ == "__main__":
     app.run(host = os.environ.get("IP"),
             port = int(os.environ.get("PORT")),
